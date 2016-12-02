9959026e0cf857511f9a1c472fa5106131447333
user <user@balin.dklab.cs.umu.se>
2016-12-02 15:41:20 +0100

    added class

--- module/kvdb.h
+++ module/kvdb.h
@@ -4,11 +4,23 @@
 #include <linux/rhashtable.h>
 #include <linux/hash.h>
 #include <linux/slab.h>
+#include <linux/module.h>  /* Needed by all kernel modules */
+#include <linux/kernel.h>  /* Needed for loglevels (KERN_WARNING, KERN_EMERG, KERN_INFO, etc.) */
+#include <linux/init.h>    /* Needed for __init and __exit macros. */
+
+#include <linux/device.h>         // Header to support the kernel Driver Model
+#include <linux/fs.h>             // Header for the Linux file system support
+#include <asm/uaccess.h>          // Required for the copy to user function
+
+
+MODULE_LICENSE("GPL");            ///< The license type -- this affects available functionality
+MODULE_AUTHOR("VSJ");    ///< The author -- visible when you use modinfo
+MODULE_DESCRIPTION("A simple Linux char driver for the BBB");  ///< The description -- see modinfo
+MODULE_VERSION("0.1");
+
  struct kvdbHolder 
  {
  	int noOfObjects;
-
-
  };
 
  struct KVDB_OBJECT 

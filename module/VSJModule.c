068d1ce8f812ca527a802edb167347ffca91f570
Victor Lundgren <dv14vln@cs.umu.se>
2017-01-18 11:08:36 +0100

    removed read-write semaphore

--- module/VSJModule.c
+++ module/VSJModule.c
@@ -23,7 +23,6 @@ static int    numberOpens = 0;              ///< Counts the number of times the 
 static struct class*  charClass  = NULL; ///< The device-driver class struct pointer
 static struct device* charDevice = NULL; ///< The device-driver device struct pointer
 static struct rhashtable *ht, *keytable;
-static struct rw_semaphore sem;
 
 static struct rhashtable_iter *iter;       // to iterate ht
 static struct hashed_object **saver;    // to list ht
@@ -126,7 +125,6 @@ static int __init onload(void) {
    }
    mutex_init(&save_mutex);
    mutex_init(&del_mutex);
-   init_rwsem(&sem);
    printk(KERN_INFO "VSJModule: device class created correctly\n");
 
     return 0;
@@ -231,22 +229,19 @@ static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *of
    }
    key = keyobj->key;
    kfree(keyobj);
-   down_read(&sem);
    rcu_read_lock();
    obj = KVDB_lookup(ht, &key, params);
    if(obj == NULL) {
-       up_read(&sem);
+   	   rcu_read_unlock();
        return -ENOKEY;
    }
    cplen = len > obj->size ? obj->size : len;
    err = copy_to_user(buffer, obj->value, cplen);
    if(err) {
        rcu_read_unlock();
-       up_read(&sem);
        return -EFAULT;
    }
    rcu_read_unlock();
-   up_read(&sem);
    return cplen;
 }
 
@@ -473,7 +468,6 @@ static int KVDB_remove (int *key){
     if(obj == NULL){
         return -ENOENT;
     }
-    down_write(&sem);
     ret = rhashtable_remove_fast(ht, &(obj->node), params);
     if(ret == 0){
         synchronize_rcu();
@@ -481,7 +475,6 @@ static int KVDB_remove (int *key){
         kfree(obj);
     }
     mutex_unlock(&del_mutex);
-    up_write(&sem);
     return ret;
 }
 
